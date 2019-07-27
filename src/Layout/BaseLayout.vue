6fb2a9a984153fc1a4ac128f4e8fa4c86b513ef7
jiachennan <jiachennanjj@126.com>
2019-07-27 15:34:03 +0800

    devepole

--- src/Layout/BaseLayout.vue
+++ src/Layout/BaseLayout.vue
@@ -1,11 +1,16 @@
 <template>
-    
+  <!-- <router-view></router-view> -->
+  <div>{{msg}}</div>
 </template>
 <script>
 export default {
-    
-}
+  name: "BaseLayout",
+  data() {
+    return {
+      msg: "good"
+    };
+  }
+};
 </script>
 <style>
-
 </style>
